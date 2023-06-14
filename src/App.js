c4812f3f202a942b962886dadfadbeb5ad020462
71raziya <omarovaeliza3@gmail.com>
2023-06-14 21:58:53 +0600

    todo3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+import './style.css';
+import { useState, useEffect} from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+     setTodoList(data.filter(item => {
+      return !item.deleted
+     }))
+  }, [data])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <Header data = {data} setData = {setData}/>
+      <Main todoList = {todoList}/>
+      <Footer/>
     </div>
   );
 }
